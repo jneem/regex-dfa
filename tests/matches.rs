c02342b102e5f7b597594b11d8d28716fea94b48
Joe Neeman <joeneeman@gmail.com>
2016-01-11 20:53:53 +0100

    Update copyright.

--- tests/matches.rs
+++ tests/matches.rs
@@ -1,6 +1,5 @@
-// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
+// Copyright 2014 The Rust Project Developers.
+// Copyright 2015-2016 Joe Neeman.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
